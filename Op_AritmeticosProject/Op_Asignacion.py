0ed062cbfd08ea64f463fb6b913c0f87beecdfa6
Andres Correa <andrescorrea0599@gmail.com>
2024-09-26 16:49:11 -0500

    Se crea el proyecto con los ejemplos de los operadores

---
+++ Op_AritmeticosProject/Op_Asignacion.py
@@ -0,0 +1,31 @@
+#Ejemplos de Op de asignaciones
+
+var1 = 5
+var2 = 12
+var3 = 8
+var4 = 14
+var5 = 6
+var6 = 8
+
+var1+= 3 #var1 = var1 + 3 asignacion con suma
+
+print(f"Asignacion con suma: {var1} ")
+
+var2 -= 3
+
+print(f"Asginacion con resta {var2} ")
+
+var3 *= 3
+
+print(f"Asginacion con * {var3}")
+
+var4 /= 3
+
+print(f"asignacion con / {var4}")
+
+var5 %= 3
+
+print(f"asignacion con % {var5}")
+
+var6 //=3
+print(f"Asignacion con // {var6}")
---
+++ Op_AritmeticosProject/Op_Comparacion.py
@@ -0,0 +1,11 @@
+#Ejemplo Operadores de comparacion
+
+m = int(input(" ingrese el valor de m "))
+n = int(input(" ingrese el valor de n "))
+
+print( m == n ) #igualdad
+print( m > n ) 
+print( m < n )
+print( m >= n )
+print( m <= n )
+print( m != n )
